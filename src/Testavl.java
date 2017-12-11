5e1722541e4f6e8a3ff840cad2e0d7ae5a1a867e
ushallnotpas <zhanghanfei1991@hotmail.com>
2017-12-11 14:44:07 -0500

    Initial commit

---
+++ src/Testavl.java
@@ -0,0 +1,11 @@
+public class Testavl {
+    public static void main(String[]args){
+
+        AVLtree<Integer>= new AVLTree<integer>();
+        for (int i=1;i<=100;i++){
+            tree.insert(i);
+        }
+
+     tree.displayPath();   
+    }
+}
